5ffc8e1118ec25502b4b694f859e8b93dcbd230f
steelydylan <ess_president@me.com>
2017-06-15 13:59:36 +0900

    added template

--- src/core/index.js
+++ src/core/index.js
@@ -10,6 +10,17 @@ class MiniEditor extends aTemplate {
 
   constructor(selector, settings) {
     super();
+		this.data = util.extend({}, defaults, settings);
+		this.id = this._getUniqId();
+		this.addTemplate(this.id, template);
+		const ele = document.querySelector(selector);
+		const html = `<div data-id='${this.id}'></div>`;
+		util.before(selector, html);
+		this.update();
+  }
+
+ _getUniqId() {
+    return (Date.now().toString(36) + Math.random().toString(36).substr(2, 5)).toUpperCase();
   }
 
 }
