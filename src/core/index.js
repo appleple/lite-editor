bf172d6efd0e8a6574d428f35821595c5d76d640
堀悟大 <godai@horisatorudainoMacBook-Pro.local>
2017-06-23 11:24:01 +0900

    added inserttag feature

--- src/core/index.js
+++ src/core/index.js
@@ -7,33 +7,41 @@ const entities = new Entities();
 const toMarkdown = require('to-markdown');
 
 const defaults = {
-	mode: 'html',
-	classNames: {
-		MiniEditor: 'minieditor'
-	},
-	message: {
-		addLinkBtn: 'add link',
-		addLinkTitle: 'Add Link',
-		addItalicBtn: 'add italic',
-	}
+  mode: 'html',
+  classNames: {
+    MiniEditor: 'minieditor',
+    MiniEditorBtn: 'minieditor-btn',
+    MiniEditorBtnGroup: 'minieditor-btn-group'
+  },
+  message: {
+    addLinkTitle: 'Add Link',
+    addLinkBtn: 'add link',
+    addItalicBtn: 'add italic',
+    addEmBtn: 'emphasis1',
+    addStrongBtn: 'emphasis2'
+  },
+  useLink: true,
+  useEm: true,
+  useStrong: true,
+  useItalic: false
 }
 
 class MiniEditor extends aTemplate {
 
   constructor(ele, settings) {
     super();
-		this.data = util.extend({}, defaults, settings);
-		this.id = this._getUniqId();
-		this.addTemplate(this.id, template);
-		const selector = typeof ele === 'string' ? document.querySelector(ele) : ele;
-		this.selector = selector;
-		const html = `<div data-id='${this.id}'></div>`;
-		selector.style.display = 'none';
-		util.before(selector, html);
-		this.update();
+    this.data = util.extend({}, defaults, settings);
+    this.id = this._getUniqId();
+    this.addTemplate(this.id, template);
+    const selector = typeof ele === 'string' ? document.querySelector(ele) : ele;
+    this.selector = selector;
+    const html = `<div data-id='${this.id}'></div>`;
+    selector.style.display = 'none';
+    util.before(selector, html);
+    this.update();
   }
 
- 	_getUniqId() {
+  _getUniqId() {
     return (Date.now().toString(36) + Math.random().toString(36).substr(2, 5)).toUpperCase();
   }
 
@@ -41,44 +49,58 @@ class MiniEditor extends aTemplate {
     return document.querySelector(`[data-id='${this.id}'] ${query}`);
   }
 
-	addLink() {
-		const data = this.data;
-		const mode = data.mode;
-		const url = prompt(data.message.addLinkTitle, 'http://');
-		if (mode === 'html') {
-		 	document.execCommand('createlink', true, url);
-		} else if (mode === 'markdown') {
-			const selection = document.getSelection();
-			const insertText = `[${selection}](${url}]`;
-			document.execCommand('insertText', false, insertText);
-		}
-	}
+  addLink() {
+    const data = this.data;
+    const mode = data.mode;
+    const url = prompt(data.message.addLinkTitle, 'http://');
+    if (mode === 'html') {
+      document.execCommand('createlink', true, url);
+    } else if (mode === 'markdown') {
+      const selection = document.getSelection();
+      const insertText = `[${selection}](${url}]`;
+      document.execCommand('insertText', false, insertText);
+    }
+  }
 
-	addItalic() {
-		document.execCommand('italic');
-	}
+  addItalic() {
+    document.execCommand('italic');
+  }
 
-	addBold() {
-		document.execCommand('bold');
-	}
+  addBold() {
+    document.execCommand('bold');
+  }
 
-	unlink() {
-		document.execCommand('unlink');
-	}
+  unlink() {
+    document.execCommand('unlink');
+  }
 
-	onUpdated() {
-		this.onInput();
-	}
+  insertTag(tag) {
+    const data = this.data;
+    const mode = data.mode;
+    if (mode === 'html') {
+      const selection = document.getSelection();
+      const insertHtml = `<${tag}>${selection}</${tag}>`;
+      document.execCommand('insertHtml', false, insertHtml);
+    }
+  }
 
-	onInput() {
-		const editor = this._getElementByQuery(`.${this.data.classNames.MiniEditor}`);
-		this.data.value = entities.decode(editor.innerHTML);
-		this.selector.value = this.data.value;
-	}
+  onUpdated() {
+    this.onInput();
+  }
 
-	changeMode(mode) {
-		this.data.mode = mode;
-	}
+  onInput() {
+    const editor = this._getElementByQuery(`.${this.data.classNames.MiniEditor}`);
+    this.data.value = entities.decode(editor.innerHTML);
+    this.selector.value = this.data.value;
+  }
+
+  onPaste() {
+    
+  }
+
+  changeMode(mode) {
+    this.data.mode = mode;
+  }
 }
 
 module.exports = MiniEditor;
