3a40a56edf55f7d381a99152bc986332939f7279
堀悟大 <godai@horisatorudainoMacBook-Pro.local>
2017-07-11 17:33:05 +0900

    v0.0.14

--- js/jquery-simple-wysiwyg.min.js
+++ js/jquery-simple-wysiwyg.min.js
@@ -6,7 +6,7 @@
  *   license: MIT (http://opensource.org/licenses/MIT)
  *   author: appleple
  *   homepage: http://developer.a-blogcms.jp
- *   version: 0.0.13
+ *   version: 0.0.14
  *
  * a-template:
  *   license: MIT (http://opensource.org/licenses/MIT)
