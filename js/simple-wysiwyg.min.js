bc6edca6f36934cd3e8b480c050e83aa1ae68d32
Hori Godai <ess_president@me.com>
2017-08-17 19:15:43 +0900

    v0.1.0

--- js/simple-wysiwyg.min.js
+++ js/simple-wysiwyg.min.js
@@ -6,7 +6,7 @@
  *   license: MIT (http://opensource.org/licenses/MIT)
  *   author: appleple
  *   homepage: http://developer.a-blogcms.jp
- *   version: 0.0.16
+ *   version: 0.1.0
  *
  * a-template:
  *   license: MIT (http://opensource.org/licenses/MIT)
