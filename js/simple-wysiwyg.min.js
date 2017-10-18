ebe6df8bf159069f60b7c82cf2ad607b1511be5e
Hori Godai <ess_president@me.com>
2017-10-18 16:35:54 +0900

    bumping version

--- js/simple-wysiwyg.min.js
+++ js/simple-wysiwyg.min.js
@@ -6,7 +6,7 @@
  *   license: MIT (http://opensource.org/licenses/MIT)
  *   author: appleple
  *   homepage: http://developer.a-blogcms.jp
- *   version: 0.2.3
+ *   version: 0.2.4
  *
  * a-template:
  *   license: MIT (http://opensource.org/licenses/MIT)
