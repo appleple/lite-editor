de76683cfc0dd6b9e2cc6dbac097cae311d2c420
Hori Godai <ess_president@me.com>
2018-03-12 11:36:29 +0900

    refactor getSelection method

--- src/lib/util.js
+++ src/lib/util.js
@@ -80,7 +80,7 @@ export const getSelection = (ele) => {
     return window.getSelection();
   } else if (document.getSelection && document.getSelection().toString()) {
     return document.getSelection();
-  } else {
+  } else if (ele && ele.selectionStart) {
     return ele.value.substr(ele.selectionStart, ele.selectionEnd - ele.selectionStart);
   }
   return '';
