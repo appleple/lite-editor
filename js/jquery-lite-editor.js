3bd13fdecef1844e6526569abc5daaf10cdfc0b3
Hori Godai <ess_president@me.com>
2018-03-12 11:38:36 +0900

    v1.6.10

--- js/jquery-lite-editor.js
+++ js/jquery-lite-editor.js
@@ -6,7 +6,7 @@
  *   license: MIT (http://opensource.org/licenses/MIT)
  *   author: appleple
  *   homepage: http://developer.a-blogcms.jp
- *   version: 1.6.9
+ *   version: 1.6.10
  *
  * a-template:
  *   license: MIT (http://opensource.org/licenses/MIT)
