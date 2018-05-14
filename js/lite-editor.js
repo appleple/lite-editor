6d9268abf98eba0cee6327684a1fa97cc061ba88
Hori Godai <ess_president@me.com>
2018-05-14 16:55:42 +0900

    v1.6.33

--- js/lite-editor.js
+++ js/lite-editor.js
@@ -6,7 +6,7 @@
  *   license: MIT (http://opensource.org/licenses/MIT)
  *   author: appleple
  *   homepage: http://developer.a-blogcms.jp
- *   version: 1.6.32
+ *   version: 1.6.33
  *
  * a-template:
  *   license: MIT (http://opensource.org/licenses/MIT)
