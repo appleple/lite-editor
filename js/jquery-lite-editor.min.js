32b97fdae02b99e74513b0ea9b2c53cc14cde3ca
Hori Godai <ess_president@me.com>
2018-06-08 15:34:50 +0900

    package security fix

--- js/jquery-lite-editor.min.js
+++ js/jquery-lite-editor.min.js
@@ -6,7 +6,7 @@
  *   license: MIT (http://opensource.org/licenses/MIT)
  *   author: appleple
  *   homepage: http://developer.a-blogcms.jp
- *   version: 1.6.37
+ *   version: 1.6.38
  *
  * a-template:
  *   license: MIT (http://opensource.org/licenses/MIT)
