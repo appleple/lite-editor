101a3b4f93a4a0198503cf84260711425e6d276a
atsu666 <ito@appleple.com>
2021-06-17 15:57:04 +0900

    v1.6.50

--- js/lite-editor.min.js
+++ js/lite-editor.min.js
@@ -6,7 +6,7 @@
  *   license: MIT (http://opensource.org/licenses/MIT)
  *   author: appleple
  *   homepage: http://developer.a-blogcms.jp
- *   version: 1.6.49
+ *   version: 1.6.50
  *
  * a-template:
  *   license: MIT (http://opensource.org/licenses/MIT)
